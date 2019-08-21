0fb81fea7ea03cbed702fe1b4b41e5052f3a7714
mostafaayoub97 <54358998+mostafaayoub97@users.noreply.github.com>
2019-08-21 15:36:58 +0200

    bookApp
    book system via firebase and vue.js

---
+++ App.vue
@@ -0,0 +1,14 @@
+<template>
+<div id="app">
+<book />
+</div>
+</template>
+<script>
+import book from "@/components/book.vue";
+export default {
+    name:'App',
+    components:{
+       book
+    }
+}
+</script>
